c79f431e461d105368462ac9f2532374d416ff8a
Sam Sam <samsface@gmail.com>
2025-07-31 01:00:23 +0200

    adding npc

---
+++ boomer-shooter/game/npc/npc.gd
@@ -0,0 +1,60 @@
+@tool
+extends Node3D
+class_name NPC
+
+@export var moving_to:Node3D
+@export var target:Node3D : 
+	set = set_target
+@export var speed := 1.0
+var speed_scale := 1.0
+
+@onready var animation_tree:AnimationTree = $AnimationTree
+
+func set_target(node:Node3D) -> void:
+	target = node
+	if not is_node_ready():
+		return
+		
+	if target:
+		if target.has_node("%Head"):
+			%LookAtModifier3D.target_node = target.get_node("%Head").get_path()
+		else:
+			%LookAtModifier3D.target_node = target.get_path()
+			
+func _physics_process(delta: float) -> void:
+	if moving_to:
+		global_position = global_position.move_toward(moving_to.global_position, delta * speed * speed_scale)
+
+	if target:
+		if moving_to:
+			var world_direction = global_position.direction_to(moving_to.global_position)
+			var forward = global_transform.basis.z
+			var right = global_transform.basis.x
+			var local_x = right.dot(world_direction)
+			var local_y = forward.dot(world_direction)
+			var blend_vector = Vector2(local_x, local_y)
+			blend_vector *= speed_scale
+			animation_tree.set("parameters/MoveDirection/blend_position", blend_vector)
+
+		look_at_node_y_axis_lerp(target, delta * 0.5)
+
+
+func look_at_node_y_axis_lerp(target_node: Node3D, delta: float, speed: float = 5.0) -> void:
+	var self_position = global_transform.origin
+	var target_position = target_node.global_transform.origin
+
+	# Get direction to target on the XZ plane
+	var direction = target_position - self_position
+	direction.y = 0
+	direction = direction.normalized()
+
+	if direction.length() == 0:
+		return  # Avoid NaN if both nodes are in the same place
+
+	# Calculate desired yaw
+	var target_yaw = atan2(direction.x, direction.z)
+	var current_yaw = global_rotation.y
+
+	# Smoothly interpolate towards target yaw
+	var new_yaw = lerp_angle(current_yaw, target_yaw, clamp(delta * speed, 0.0, 1.0))
+	global_rotation.y = new_yaw
---
+++ boomer-shooter/game/npc/npc_enemy.gd
@@ -0,0 +1,54 @@
+@tool
+extends NPC
+class_name NPCEnemy
+
+class State extends Node:
+	func move_to(state:State) -> void:
+		queue_free()
+		var p := get_parent()
+		p.remove_child(self)
+		p.add_child(state)
+
+
+class StateIdle extends State:
+	func _ready() -> void:
+		get_parent().get_node("%Vision").body_entered.connect(_body_entered_vision)
+
+	func _body_entered_vision(body) -> void:
+		var next_state = StateSpottedEnemy.new()
+		next_state.enemy = body
+		move_to(next_state)
+		
+
+class StateSpottedEnemy extends State:
+	var enemy:Node3D
+	
+	func _ready() -> void:
+		get_parent().animation_tree.set("parameters/Special/transition_request", "Panic")
+		get_parent().target = enemy
+		await get_tree().create_timer(2.0).timeout
+		get_parent().animation_tree.set("parameters/Special/transition_request", "Moving")
+
+		var next_state = StateAttacking.new()
+		next_state.enemy = enemy
+		move_to(next_state)
+
+
+class StateAttacking extends State:
+	var enemy:Node3D
+	
+	func _ready() -> void:
+		get_parent().target = enemy
+		get_parent().moving_to = enemy
+		
+	func _physics_process(delta: float) -> void:
+		var ds = enemy.global_position.distance_squared_to(get_parent().global_position)
+		
+		get_parent().speed_scale = lerp(0.0, 1.0, clamp(ds / 3.0, 0.0, 1.0))
+		
+		if ds < 1.0:
+			get_parent().speed_scale = 0.0
+
+
+func _ready() -> void:
+	add_child(StateIdle.new())
