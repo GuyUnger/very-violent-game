27b75f949cf7ec68414a76a94c48dde3094c1567
Guy <guyunger@gmail.com>
2025-08-03 20:12:18 +0800

    .

--- boomer-shooter/game/misc/finish.gd
+++ boomer-shooter/game/misc/finish.gd
@@ -4,6 +4,24 @@ extends Area3D
 
 var entered: bool = false
 
+
+func _ready() -> void:
+	Main.instance.enemy_killed.connect(_on_enemy_killed)
+	
+	await get_tree().process_frame
+	_on_enemy_killed()
+
+
+func _on_enemy_killed() -> void:
+	await get_tree().process_frame
+	if Main.instance.enemies_left == 0:
+		open()
+
+
+func open() -> void:
+	%Light.visible = true
+
+
 func _on_body_entered(body:Node3D) -> void:
 	if entered:
 		return
@@ -14,4 +32,3 @@ func _on_body_entered(body:Node3D) -> void:
 	await Transition.close()
 	get_tree().change_scene_to_file(next_level)
 	Transition.open()
-	
--- boomer-shooter/stages/main.gd
+++ boomer-shooter/stages/main.gd
@@ -1,11 +1,17 @@
 class_name Main
 extends Node3D
 
+signal enemy_killed
+
 static var instance
 
 var source_id := 1
 
 @export var time: float = 30.0
+
+var total_enemies: int
+var enemies_left: int
+
 
 func _init() -> void:
 	instance = self
@@ -14,9 +20,19 @@ func _init() -> void:
 func _ready() -> void:
 	EventStore.register_source(source_id, self)
 	get_tree().paused = true
+	
+	await get_tree().process_frame
+	total_enemies = get_tree().get_nodes_in_group("npc_enemies").size()
+	enemies_left = total_enemies
+	enemy_killed.connect(_on_enemy_killed)
 	await get_tree().create_timer(0.3).timeout
 	get_tree().paused = false
-	
+
+func _on_enemy_killed() -> void:
+	enemies_left = 0
+	for enemy in get_tree().get_nodes_in_group("npc_enemies"):
+		if enemy.health > 0:
+			enemies_left +=1
 
 static var player: Player
 
