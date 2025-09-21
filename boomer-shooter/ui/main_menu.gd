c964b9ea26c92f1af44d94535698f3f3f0abfef2
Guy <guyunger@gmail.com>
2025-09-21 22:19:15 +0800

    .

--- boomer-shooter/ui/main_menu.gd
+++ boomer-shooter/ui/main_menu.gd
@@ -3,6 +3,18 @@ extends Control
 
 func _ready() -> void:
 	set_menu(%Main)
+	Transition.play_end()
+	
+	add_button_sounds(self)
+
+
+func add_button_sounds(parent: Node) -> void:
+	for child in parent.get_children():
+		if child is Button:
+			child.mouse_entered.connect(%AudioHover.play)
+			child.pressed.connect(%AudioSelect.play)
+		else:
+			add_button_sounds(child)
 
 
 func _on_button_play_pressed() -> void:
@@ -15,6 +27,8 @@ func _on_button_back_pressed() -> void:
 
 func set_menu(menu: Control) -> void:
 	for child in get_children():
+		if child is AudioStreamPlayer:
+			continue
 		child.visible = child == menu
 
 
